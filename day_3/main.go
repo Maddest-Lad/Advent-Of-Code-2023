f46941dc0298cbea486508a582d802d5f6caa228
Maddest-Lad <samharris23@live.com>
2023-12-05 13:22:04 -0800

    saving work in progress

--- day_3/main.go
+++ day_3/main.go
@@ -26,10 +26,32 @@ func main() {
 	}
 
 	// Logic to Scan For Chars
-	for _, row := range grid {
-		for _, char := range row {
-			fmt.Printf(string(char))
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			fmt.Println(string(grid[i][j]))
+			var neighbors = getNeighbors(grid, i, j)
+			var _ = neighbors
 		}
-		fmt.Println()
 	}
+}
+
+func getNeighbors(grid [][]rune, row int, col int) []rune {
+	var neighbors []rune
+
+	// Prevent Out of Index Access
+	rowLimit := len(grid)
+	colLimit := len(grid[0])
+
+	// Using Min/Max to Avoid Boundaries
+	for i := utils.MaxOf(0, row-1); i < utils.MinOf(row+1, rowLimit); i++ {
+		for j := utils.MaxOf(0, col-1); j < utils.MinOf(col+1, colLimit); j++ {
+			fmt.Printf("[%d][%d] \n", i, j)
+			if i != row && j != col {
+				// Ignore Middle
+				neighbors = append(neighbors, grid[i][j])
+			}
+		}
+	}
+	fmt.Printf("grid[%d][%d] %s: %s", row, col, string(grid[row][col]), string(neighbors))
+	return neighbors
 }
